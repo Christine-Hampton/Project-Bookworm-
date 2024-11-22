910e1bb12fe5c02b17cfbdec3a2d65914327d69f
Christine-Hampton <champton86@ivytech.edu>
2024-11-22 15:32:51 -0500

    Update Main.py
    Updates main,py to match GUI.py 
    removed 1 line and adjust "run" to mainloop"

--- Main.py
+++ Main.py
@@ -2,6 +2,5 @@ from GUI import BookstoreApp
 import tkinter as TK
 
 if __name__ == "__main__":
-    root = TK.Tk()
-    app = BookstoreApp(root)
-    app.run()
+    app = BookstoreApp()
+    app.mainloop
