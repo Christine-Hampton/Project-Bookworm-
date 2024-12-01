1e4cbb1920bfd8154a5101334bbf0999d2b79735
Christine-Hampton <champton86@ivytech.edu>
2024-12-01 00:05:17 -0500

    Update customer.py
    updated to add bookstore.db

--- customer.py
+++ customer.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Customer:
     def __init__(self, first_name, last_name, customer_id, email, phone_number):
         self.first_name = first_name 
@@ -5,6 +7,12 @@ class Customer:
         self.customer_id = customer_id
         self.email = email
         self.phone_number = phone_number 
+     
+    def save_to_db(self):
+        connection = sqlite3.connect('bookstore.db')
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO customers (name, customer_id, email, phone_number) VALUES (?, ?, ?, ?,)'
+                       (self.name, self.customer_id, self.email, self.phone_number,))
 
     def add_customer(self):
         # add customer to data base 
