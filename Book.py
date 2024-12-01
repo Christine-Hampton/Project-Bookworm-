2a3865b955ae9611591405b48cfbc7a8698b6bd6
Christine-Hampton <champton86@ivytech.edu>
2024-12-01 00:03:12 -0500

    Update Book.py
    updated to insert bookstore.db

--- Book.py
+++ Book.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Book: 
     def __init__(self, title, author, genre, isbn, price, quantity):
         self.title = title
@@ -6,6 +8,15 @@ class Book:
         self.isbn = isbn
         self.price = price
         self. quantity = quantity
+   
+     def save_to_db(self):
+        connection = sqlite3.connect('bookstore.db')
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO books (title, author, isbn, price, quantity) VALUES (?, ?, ?, ?, ?)',
+                        ( self.title, self.author, self.isbn, self.price, self.quantity))
+        connection.commit()
+        connection.close()
+   
 
     def add_book(self):
         #add book to inventory
